f171dfada54191fd0aea7366719495e1415b2a05
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-14 01:54:32 +0200

    Exo 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...vals) {
+
+    return arr.filter(i => !vals.includes(i))
+
+
+
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
