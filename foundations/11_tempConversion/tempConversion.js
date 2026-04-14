fda8053721bd0ee1041c89051b92831d910fe8b8
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-14 03:00:00 +0200

    Exo 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+    Celsius = (fahr - 32) * (5/9)
+    return Math.round(Celsius * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+    Fahrenheit = (cels * (9/5)) + 32
+    return Math.round(Fahrenheit * 10) /10
 };
 
 // Do not edit below this line
