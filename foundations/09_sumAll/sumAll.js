0436b0c609c5ba7465d590f7d5ce9dd68697b65e
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-14 02:19:35 +0200

    Exo 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,12 @@
-const sumAll = function() {
+const sumAll = function(int_1, int_2) {
+
+    arr = [];
+    if (int_1 < 0 || int_2 < 0 || !Number.isInteger(int_1) || !Number.isInteger(int_2) ) return "ERROR";
+
+    for (let i = Math.min(int_1, int_2); i<=Math.max(int_1,int_2); i++){
+        arr.push(i)
+    };
+    return arr.reduce((total, item) => total + item );
 
 };
 
