3e0e4af146bf1350e04a5566cb50f78da7bda968
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-17 00:51:00 +0200

    Exo 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    return array.reduce((total, item) => total + item,0)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    return array.reduce((total, item) => total * item, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+    return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    if (a === 0 ){
+        return 1
+    }
+    else{
+        let fact = 1
+        for (let i = a; i> 0; i--){
+            fact *= i
+        }
+        return fact
+    }
 };
 
 // Do not edit below this line
