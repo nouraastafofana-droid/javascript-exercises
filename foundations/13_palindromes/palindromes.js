5aa9f9c4fe949ff3893de68e7fc40b7deb377a48
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-17 01:30:27 +0200

    Exo 13 avec aide

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (entree) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let nettoyage = entree.toLowerCase().split('').filter(car => alphanumerical.includes(car)).join("");
+
+    let palindrome = nettoyage.split("").reverse().join("");
+
+
+    if (nettoyage === palindrome){
+        return true
+    }
+    else{
+        return false
+    }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
