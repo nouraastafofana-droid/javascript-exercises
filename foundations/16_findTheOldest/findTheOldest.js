123fd62e258d21e32cf7dea6af054ec55366fa78
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-17 02:50:44 +0200

    Exo 16 avec reduce

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    const age = function(p){
+        if (p.yearOfDeath ){
+            return p.yearOfDeath - p.yearOfBirth
+        }
+        else{
+            return new Date().getFullYear() - p.yearOfBirth
+        }
+    }
+
+    return people.reduce( (oldest, person) => {
+        if (age(oldest) > age(person)  ) { return oldest}
+        else { return person }
+
+    }
+ )
 
 };
 
