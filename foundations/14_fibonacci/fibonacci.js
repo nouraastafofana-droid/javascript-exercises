c17dcc6e39bdfa3dbd2adbfe382bbb05800ef880
FOFANA Noura Asta DEC/DECP <nouraasta.fofana@orange.com>
2026-04-17 02:12:03 +0200

    Exo 14  avec recursivité et aide

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,31 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = +num
+    if (num < 0){
+        return "OOPS"
+    }
+    else if (num === 0){
+        return 0
+    }
+    else if (num === 1){
+        return 1
+    }
+    else{
+        let first = 0;
+        let sec = 1;
+        for (let i = 2; i<= num; i++){
+            let current = first + sec;
+            first = sec;
+            sec = current;
 
+
+
+
+        }
+
+        return sec;
+
+
+    }
 };
 
 // Do not edit below this line
